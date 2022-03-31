d87d5c3442b537a5c6610037e97fa7db7915002e
Chris Bruce <chris@chrisbruce.com>
2022-03-31 19:51:04 +0200

    container trait

---
+++ blockvisord/src/containers.rs
@@ -0,0 +1,57 @@
+use anyhow::Result;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait NodeContainer {
+    /// Creates a new container with `id`.
+    async fn create(id: &str) -> Result<Self>
+    where
+        Self: Sized;
+
+    /// Returns the container's `id`.
+    fn id(&self) -> String;
+
+    /// Starts the container.
+    async fn start(&self) -> Result<()>;
+
+    /// Returns the state of the container.
+    async fn state(&self) -> Result<()>;
+
+    /// Kills the running container.
+    async fn kill(&self) -> Result<()>;
+
+    /// Deletes the container.
+    async fn delete(&self) -> Result<()>;
+}
+
+pub struct LinuxNode {
+    pub id: String,
+}
+
+#[async_trait]
+impl NodeContainer for LinuxNode {
+    async fn create(_id: &str) -> Result<Self> {
+        unimplemented!()
+    }
+
+    fn id(&self) -> String {
+        self.id.to_owned()
+    }
+
+    async fn start(&self) -> Result<()> {
+        unimplemented!()
+    }
+
+    async fn state(&self) -> Result<()> {
+        unimplemented!()
+    }
+
+    async fn kill(&self) -> Result<()> {
+        unimplemented!()
+    }
+
+    /// Deletes the container.
+    async fn delete(&self) -> Result<()> {
+        unimplemented!()
+    }
+}
--- blockvisord/src/main.rs
+++ blockvisord/src/main.rs
@@ -1,3 +1,17 @@
+use core::time;
+use fork::{daemon, Fork};
+
+mod containers;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(Fork::Child) = daemon(false, false) {
+        work();
+    }
+}
+
+fn work() {
+    loop {
+        println!("Hello");
+        std::thread::sleep(time::Duration::from_secs(2));
+    }
 }
