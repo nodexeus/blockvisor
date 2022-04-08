7876ed3d0b3d0277996788d4031b461022e35a45
Adrian Bece <adrian@prototyp.digital>
2022-04-08 09:49:20 +0200

    Fix custom media query compilation issue

--- frontend/src/modules/authentication/store/auth.ts
+++ frontend/src/modules/authentication/store/auth.ts
@@ -2,4 +2,7 @@ import { session } from '$app/stores';
 import { derived } from 'svelte/store';
 
 // Simple wrapper around session store for ease of use
-export const user = derived(session, ($session) => $session.user);
+export const user = derived(session, ($session) => {
+  console.log($session);
+  return $session.user;
+});
