aa1cccb9962b5362e747d1cbd109e4e7979fee4a
Adrian Bece <adrian@prototyp.digital>
2022-04-08 09:51:58 +0200

    Revert session change

--- frontend/src/modules/authentication/store/auth.ts
+++ frontend/src/modules/authentication/store/auth.ts
@@ -2,7 +2,4 @@ import { session } from '$app/stores';
 import { derived } from 'svelte/store';
 
 // Simple wrapper around session store for ease of use
-export const user = derived(session, ($session) => {
-  console.log($session);
-  return $session.user;
-});
+export const user = derived(session, ($session) => $session.user);
