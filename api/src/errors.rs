89c7c8b89f2ed7adf6973d038075ab40367900c5
Chris Bruce <chris@chrisbruce.com>
2022-04-18 12:54:10 +0200

    fix error type

--- api/src/errors.rs
+++ api/src/errors.rs
@@ -8,15 +8,15 @@ use serde_json::json;
 /// Wrapper Error enum used to provide a consistent [`IntoResponse`] target for
 /// request handlers that return inner domain Error types.
 #[derive(Debug, thiserror::Error)]
-pub enum AppError {
+pub enum ApiError {
     #[error("database error")]
     SqlError(#[from] sqlx::Error),
 }
 
-impl IntoResponse for AppError {
+impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
         let (status, message) = match self {
-            AppError::SqlError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "database error"),
+            ApiError::SqlError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "database error"),
         };
 
         let body = Json(json!({ "error": message }));
