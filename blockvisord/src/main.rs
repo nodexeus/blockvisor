4dbc61cc1a04bda1cb516b08df083e0481943f45
Alexey Suslov <alexey.suslov@gmail.com>
2022-05-04 16:21:58 +0200

    Add bv cli commands

--- blockvisord/src/main.rs
+++ blockvisord/src/main.rs
@@ -1,11 +1,26 @@
+use clap::Parser;
+use cli::{App, Command};
 use core::time;
 use fork::{daemon, Fork};
 
+mod cli;
 mod containers;
 
 fn main() {
-    if let Ok(Fork::Child) = daemon(false, false) {
-        work();
+    let args = App::parse();
+    println!("{:?}", args);
+
+    match args.command {
+        Command::Start(cmd_args) => {
+            if cmd_args.daemonize == true {
+                if let Ok(Fork::Child) = daemon(false, false) {
+                    work();
+                }
+            } else {
+                work()
+            }
+        }
+        _ => {}
     }
 }
 
