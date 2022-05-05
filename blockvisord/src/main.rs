fe3496d91165879c2b1f0e4bd87334f404490576
Alexey Suslov <alexey.suslov@gmail.com>
2022-05-05 21:20:35 +0200

    Add api client and some models

--- blockvisord/src/main.rs
+++ blockvisord/src/main.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 use tokio::time::{sleep, Duration};
 
 mod cli;
+mod client;
 mod containers;
 
 const CONFIG_FILE: &str = "config.toml";
@@ -81,5 +82,148 @@ async fn work(daemonized: bool) {
             println!("Stopping blockvisor");
             break;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::client::{APIClient, CommandStatusUpdate, HostInfo};
+    use chrono::{TimeZone, Utc};
+    use httpmock::prelude::*;
+    use serde_json::json;
+    use std::time::Duration;
+
+    #[test]
+    fn test_register_host() {
+        let server = MockServer::start();
+
+        let m = server.mock(|when, then| {
+            when.method(POST)
+                .path("/hosts")
+                .header("Content-Type", "application/json")
+                .header("authorization", "Bearer OTP")
+                .json_body(json!({
+                    "cpu_count": 4,
+                    "mem_size": 8,
+                    "hostname": "host",
+                    "disk_size": 100,
+                }));
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .json_body(json!({
+                    "host_id": "eb4e20fc-2b4a-4d0c-811f-48abcf12b89b",
+                    "token": "secret_token"
+                }));
+        });
+
+        let client = APIClient::new(server.base_url(), Duration::from_secs(10)).unwrap();
+        let otp = "OTP";
+        let info = HostInfo {
+            cpu_count: 4,
+            mem_size: 8,
+            hostname: "host".to_string(),
+            disk_size: 100,
+        };
+        let resp = client.register_host(otp, &info).unwrap();
+
+        assert_eq!(resp.host_id, "eb4e20fc-2b4a-4d0c-811f-48abcf12b89b");
+        assert_eq!(resp.token, "secret_token");
+
+        m.assert();
+    }
+
+    #[test]
+    fn test_get_pending_commands() {
+        let server = MockServer::start();
+
+        let token = "TOKEN";
+        let host_id = "eb4e20fc-2b4a-4d0c-811f-48abcf12b89b";
+
+        let m = server.mock(|when, then| {
+            when.method(GET)
+                .path(format!("/hosts/{}/commands/pending", host_id))
+                .header("Content-Type", "application/json")
+                .header("authorization", format!("Bearer {}", token));
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .json_body(json!([
+                  {
+                    "id": "497f6eca-6276-4993-bfeb-53cbbbba6f08",
+                    "host_id": host_id,
+                    "cmd": "restart_miner",
+                    "created_at": "2019-08-24T14:15:22Z",
+                  }
+                ]));
+        });
+
+        let client = APIClient::new(server.base_url(), Duration::from_secs(10)).unwrap();
+        let resp = client.get_pending_commands(token, host_id).unwrap();
+
+        assert_eq!(resp.len(), 1);
+        assert_eq!(resp[0].id, "497f6eca-6276-4993-bfeb-53cbbbba6f08");
+        assert_eq!(resp[0].host_id, host_id);
+        assert_eq!(resp[0].cmd, "restart_miner");
+        assert_eq!(resp[0].sub_cmd, None);
+        assert_eq!(resp[0].response, None);
+        assert_eq!(resp[0].exit_status, None);
+        assert_eq!(resp[0].created_at, Utc.ymd(2019, 8, 24).and_hms(14, 15, 22));
+        assert_eq!(resp[0].completed_at, None);
+
+        m.assert();
+    }
+
+    #[test]
+    fn test_update_command_status() {
+        let server = MockServer::start();
+
+        let token = "TOKEN";
+        let command_id = "497f6eca-6276-4993-bfeb-53cbbbba6f08";
+
+        let m = server.mock(|when, then| {
+            when.method(PUT)
+                .path(format!("/commands/{}/response", command_id))
+                .header("Content-Type", "application/json")
+                .header("authorization", format!("Bearer {}", token))
+                .json_body(json!({
+                    "response": "restarted",
+                    "exit_status": 0_i32,
+                }));
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .json_body(json!(
+                  {
+                    "id": command_id,
+                    "host_id": "eb4e20fc-2b4a-4d0c-811f-48abcf12b89b",
+                    "cmd": "restart_miner",
+                    "response": "restarted",
+                    "exit_status": 0_i32,
+                    "created_at": "2019-08-24T14:15:22Z",
+                    "completed_at": "2020-08-24T14:15:22Z",
+                  }
+                ));
+        });
+
+        let client = APIClient::new(server.base_url(), Duration::from_secs(10)).unwrap();
+        let update = CommandStatusUpdate {
+            response: "restarted".to_string(),
+            exit_status: 0,
+        };
+        let resp = client
+            .update_command_status(token, command_id, &update)
+            .unwrap();
+
+        assert_eq!(resp.id, command_id);
+        assert_eq!(resp.host_id, "eb4e20fc-2b4a-4d0c-811f-48abcf12b89b");
+        assert_eq!(resp.cmd, "restart_miner");
+        assert_eq!(resp.sub_cmd, None);
+        assert_eq!(resp.response, Some("restarted".to_string()));
+        assert_eq!(resp.exit_status, Some(0_i32));
+        assert_eq!(resp.created_at, Utc.ymd(2019, 8, 24).and_hms(14, 15, 22));
+        assert_eq!(
+            resp.completed_at,
+            Some(Utc.ymd(2020, 8, 24).and_hms(14, 15, 22))
+        );
+
+        m.assert();
     }
 }
