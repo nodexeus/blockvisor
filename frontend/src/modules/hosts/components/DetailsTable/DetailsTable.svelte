519271dc9262589e028e63202e4703f428ee33d3
Bojan Bušljeta <bojan@prototyp.digital>
2022-05-12 14:00:49 +0200

    extend host details

--- frontend/src/modules/hosts/components/DetailsTable/DetailsTable.svelte
+++ frontend/src/modules/hosts/components/DetailsTable/DetailsTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { format } from 'date-fns';
+  import { format } from 'date-fns';  
 
   import HostDataRow from './HostDataRow.svelte';
 
@@ -9,7 +9,7 @@ import { format } from 'date-fns';
   const {
   os,
   os_version,
-  created_at} = data;
+  created_at, cpu_count, disk_size, mem_size} = data;
 </script>
 
 <table class="table details-table">
@@ -19,18 +19,42 @@ import { format } from 'date-fns';
   </colgroup>
 
   <tbody>
+    {#if os}
+      <HostDataRow>
+        <svelte:fragment slot="title">OS</svelte:fragment>
+        {os}
+      </HostDataRow>
+    {/if}
+    {#if os_version}
+      <HostDataRow>
+        <svelte:fragment slot="title">OS Version</svelte:fragment>
+        {os_version}
+      </HostDataRow>
+    {/if}
+    {#if cpu_count}
     <HostDataRow>
-      <svelte:fragment slot="title">OS</svelte:fragment>
-      {os}
+      <svelte:fragment slot="title">CPU CORES</svelte:fragment>
+      {cpu_count}
     </HostDataRow>
+    {/if}
+    {#if mem_size}
     <HostDataRow>
-      <svelte:fragment slot="title">OS Version</svelte:fragment>
-      {os_version}
+      <svelte:fragment slot="title">MEMORY</svelte:fragment>
+      {mem_size && (mem_size / 1000000).toFixed(2)} GB
     </HostDataRow>
+    {/if}
+    {#if disk_size}
+    <HostDataRow>
+      <svelte:fragment slot="title">DISK SIZE</svelte:fragment>
+      {disk_size && (disk_size / 1000000000).toFixed(2)} GB
+    </HostDataRow>
+    {/if}
+    {#if created_at}
     <HostDataRow>
       <svelte:fragment slot="title">Created</svelte:fragment>
       {created_at && format(+new Date(created_at),'MM/dd/yyyy')}
     </HostDataRow>
+    {/if}
   </tbody>
 </table>
 
