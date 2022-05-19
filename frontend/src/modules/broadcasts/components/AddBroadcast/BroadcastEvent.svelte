e92781f5a982574f8233a67331255f09e9f33018
Sebastijan Dumančić <sebastijan@prototyp.digital>
2022-05-19 12:10:11 +0200

    Added new fields for Broadcast

---
+++ frontend/src/modules/broadcasts/components/AddBroadcast/BroadcastEvent.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import Checkbox from 'modules/forms/components/Checkbox/Checkbox.svelte';
+  import { useForm } from 'svelte-use-form';
+  import IconEye from 'icons/eye-12.svg';
+
+  export let name: string;
+  export let value: string;
+
+  let details: boolean = false;
+
+  function toggleDetails() {
+    details = !details;
+  }
+
+  const form = useForm();
+</script>
+
+<div>
+  <div class="add-broadcast__checkbox">
+    <Checkbox {name} field={$form?.addGateway}>
+      {value}
+    </Checkbox>
+    <button
+      type="button"
+      on:click={toggleDetails}
+      class="u-button-reset add-broadcast__action"><IconEye /></button
+    >
+  </div>
+  {#if details}
+    <pre class="code-block">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos explicabo
+      tempore, sed sequi, laudantium vel voluptates et repudiandae, debitis odit
+      modi ex quam culpa natus voluptatum enim amet nesciunt similique.
+    </pre>
+  {/if}
+</div>
+
+<style>
+  .add-broadcast__checkbox {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 16px;
+  }
+
+  .add-broadcast__action {
+    color: var(--color-border-2);
+    padding: 6px;
+    border: 1px solid var(--color-border-2);
+    border-radius: 4px;
+    transition: all 0.1s ease-out;
+
+    &:hover,
+    &:focus {
+      color: white;
+      border-color: white;
+    }
+  }
+
+  .code-block {
+    margin: 18px 0;
+    padding: 10px 18px;
+    white-space: break-spaces;
+    word-break: break-all;
+
+    background: rgba(248, 250, 246, 0.03);
+
+    border: 1px solid rgba(248, 250, 246, 0.1);
+    border-radius: 4px;
+  }
+</style>
--- frontend/src/routes/app/broadcasts/add/index.svelte
+++ frontend/src/routes/app/broadcasts/add/index.svelte
@@ -2,11 +2,20 @@
   import { fade } from 'svelte/transition';
   import AddBroadcast from 'modules/broadcasts/components/AddBroadcast/AddBroadcast.svelte';
   import { fadeDefault } from 'consts/animations';
+  import SectionDescription from 'components/SectionDescription/SectionDescription.svelte';
 </script>
 
 <section in:fade={fadeDefault} class="grid broadcast-add">
   <div class="container--small broadcast-add__wrapper">
     <h2 class="t-xxlarge-fluid">Add a Broadcast</h2>
+
+    <SectionDescription>
+      Broadcast allows you to be instantly notified of any transaction matching
+      your criteria below. Once a new block is produced on the chain, our
+      broadcast service will inspect it for any transactions matching your
+      filters below and will instantly send the json (and oracle price) to your
+      callback.
+    </SectionDescription>
 
     <AddBroadcast />
   </div>
