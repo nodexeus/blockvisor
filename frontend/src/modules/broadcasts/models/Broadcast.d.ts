b354285ec11db4acaaaa4150e990418fa4ce3b97
Bojan Bušljeta <bojan@prototyp.digital>
2022-06-01 10:21:51 +0200

    fix addresses and txn_types params when creating and editing broadcasts

--- frontend/src/modules/broadcasts/models/Broadcast.d.ts
+++ frontend/src/modules/broadcasts/models/Broadcast.d.ts
@@ -3,10 +3,10 @@ interface Broadcast {
   blockchain_id: string;
   org_id: string;
   name: string;
-  addresses: string;
+  addresses: string[];
   callback_url: string;
   auth_token: string;
-  txn_types: string;
+  txn_types: string[];
   is_active: boolean;
   last_processes_height?: number;
   created_at?: string;
