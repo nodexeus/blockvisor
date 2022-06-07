038a15fe0e7da5c86a05e256832b3e0d9f8a5c9b
Sebastijan Dumančić <sebastijan@prototyp.digital>
2022-06-07 15:45:05 +0200

    successfully create an organisation

--- frontend/src/modules/organisation/components/CreateNewOrganisation.svelte
+++ frontend/src/modules/organisation/components/CreateNewOrganisation.svelte
@@ -1,19 +1,46 @@
 <script lang="ts">
   import Button from 'components/Button/Button.svelte';
   import Modal from 'components/Modal/Modal.svelte';
+  import { toast } from 'components/Toast/Toast';
+  import { ENDPOINTS } from 'consts/endpoints';
   import Input from 'modules/forms/components/Input/Input.svelte';
   import { Hint, required, useForm } from 'svelte-use-form';
+  import { httpClient } from 'utils/httpClient';
 
   const form = useForm();
   export let handleModalClose;
   export let isModalOpen;
   export let loading: boolean = false;
+  export let loadingCreate: boolean = false;
+
+  async function handleSubmit() {
+    if (!$form.values.orgName) {
+      toast.warning('Organisation name is required');
+      return;
+    }
+
+    try {
+      const res = await httpClient.post(
+        ENDPOINTS.ORGANISATIONS.CREATE_ORGANISATION_POST,
+        {
+          name: $form.values.orgName,
+        },
+      );
+
+      if (res.status === 200) {
+        handleModalClose();
+        toast.success('Organisation created successfully');
+      }
+    } catch (error) {
+      toast.warning('Something went wrong');
+    }
+  }
 </script>
 
 <Modal id="new-org" {handleModalClose} isActive={isModalOpen} size="large">
   <svelte:fragment slot="header">Create new organisation</svelte:fragment>
 
-  <form use:form on:change={() => {}}>
+  <form use:form on:submit|preventDefault={handleSubmit}>
     <Input
       size="large"
       validate={[required]}
