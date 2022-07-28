8c57c9ed83a37b1cce997352cca6d114f6a174c7
Alexey Suslov <alexey.suslov@gmail.com>
2022-07-28 16:04:43 +0200

    Add proto related deps and build infrastructure

---
+++ build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    tonic_build::configure()
+        .build_server(false)
+        .build_client(true)
+        .compile(
+            &[
+                "proto/blockjoy/api/v1/command_flow.proto",
+                "proto/blockjoy/api/v1/command.proto",
+                "proto/blockjoy/api/v1/host_service.proto",
+                "proto/blockjoy/api/v1/host.proto",
+                "proto/blockjoy/api/v1/messages.proto",
+                "proto/blockjoy/api/v1/node_types.proto",
+                "proto/blockjoy/api/v1/node.proto",
+            ],
+            &["proto/blockjoy/api/v1"],
+        )
+        .unwrap();
+}
