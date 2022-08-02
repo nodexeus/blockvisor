025504aec5e97270c6235ced3fc633f0d0a11772
Alexey Suslov <alexey.suslov@gmail.com>
2022-08-02 12:33:50 +0200

    Add grpc client with simple token auth

--- src/grpc.rs
+++ src/grpc.rs
@@ -1,3 +1,28 @@
 pub mod pb {
     tonic::include_proto!("blockjoy.api.v1");
 }
+
+use pb::command_flow_client::CommandFlowClient;
+use tonic::codegen::InterceptedService;
+use tonic::service::Interceptor;
+use tonic::{Request, Status};
+
+pub struct AuthToken(pub String);
+
+pub type Client = CommandFlowClient<InterceptedService<tonic::transport::Channel, AuthToken>>;
+
+impl Interceptor for AuthToken {
+    fn call(&mut self, request: Request<()>) -> Result<Request<()>, Status> {
+        let mut request = request;
+        request
+            .metadata_mut()
+            .insert("authorization", self.0.parse().unwrap());
+        Ok(request)
+    }
+}
+
+impl Client {
+    pub fn with_auth(channel: tonic::transport::Channel, token: AuthToken) -> Self {
+        CommandFlowClient::with_interceptor(channel, token)
+    }
+}
