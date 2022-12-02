0472c04f80005208796f814c42b331e702929c4e
Luuk Wester <luuk@blockjoy.com>
2022-12-02 17:45:13 +0100

    place space before percentages

--- bv/src/bin/bv.rs
+++ bv/src/bin/bv.rs
@@ -171,9 +171,9 @@ async fn process_host_command(command: HostCommand) -> Result<()> {
             println!("Used cpu:       {:>13} of 100", metrics.used_cpu);
             println!("Used mem:       {:>13} bytes", metrics.used_memory);
             println!("Used disk:      {:>13} bytes", metrics.used_disk_space);
-            println!("Load (1 min):   {:>13}%", metrics.load_one);
-            println!("Load (5 mins):  {:>13}%", metrics.load_five);
-            println!("Load (15 mins): {:>13}%", metrics.load_fifteen);
+            println!("Load (1 min):   {:>13} %", metrics.load_one);
+            println!("Load (5 mins):  {:>13} %", metrics.load_five);
+            println!("Load (15 mins): {:>13} %", metrics.load_fifteen);
             println!("Network in:     {:>13} bytes", metrics.network_received);
             println!("Network out:    {:>13} bytes", metrics.network_sent);
             println!("Uptime:         {:>13} seconds", metrics.uptime);
