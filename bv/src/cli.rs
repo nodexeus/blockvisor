31627f2620f24ee511fd097a6b4dc4aca31d77d3
Alexey Suslov <alexey.suslov@gmail.com>
2022-12-20 11:07:41 +0100

    Update api url

--- bv/src/cli.rs
+++ bv/src/cli.rs
@@ -16,11 +16,11 @@ pub struct InitArgs {
     pub otp: String,
 
     /// BlockJoy API url
-    #[clap(long = "url", default_value = "https://api.blockvisor.dev")]
+    #[clap(long = "url", default_value = "https://api.dev.blockjoy.com")]
     pub blockjoy_api_url: String,
 
     /// BlockJoy registry url
-    #[clap(long = "registry", default_value = "https://api.blockvisor.dev")]
+    #[clap(long = "registry", default_value = "https://api.dev.blockjoy.com")]
     pub blockjoy_registry_url: String,
 
     /// Network interface name
