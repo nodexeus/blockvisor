b5b29ed584850237feea2cfb014f94877e337bc3
Alexey Suslov <alexey.suslov@gmail.com>
2023-03-30 09:59:41 +0200

    test: fix assert text

--- bv/tests/src/bv_online.rs
+++ bv/tests/src/bv_online.rs
@@ -30,7 +30,7 @@ fn test_bvup_unknown_otp() {
         .env("BV_ROOT", tmp_dir.as_os_str())
         .assert()
         .failure()
-        .stderr(predicate::str::contains("Record not found"));
+        .stderr(predicate::str::contains("Missing valid token"));
 }
 
 #[tokio::test]
