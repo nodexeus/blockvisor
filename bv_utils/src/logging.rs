6e57a86a1c81750d77682487012b811f6c7876bf
Michał Jaremko <msiergiej85@gmail.com>
2024-05-13 14:50:18 +0200

    chore: rollback env logs

--- bv_utils/src/logging.rs
+++ bv_utils/src/logging.rs
@@ -1,22 +1,20 @@
-use tracing_subscriber::{
-    self, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer, Registry,
-};
+use tracing_subscriber::{self, layer::SubscriberExt, util::SubscriberInitExt};
 
+#[cfg(feature = "bv_fmt_log")]
+use tracing_subscriber::{EnvFilter, Layer, Registry};
+
+#[cfg(not(feature = "bv_fmt_log"))]
+pub fn setup_logging() {
+    if let Ok(journald) = tracing_journald::layer() {
+        let _ = tracing_subscriber::registry().with(journald).try_init();
+    }
+}
+
+#[cfg(feature = "bv_fmt_log")]
 pub fn setup_logging() {
     let fmt = <tracing_subscriber::fmt::Layer<Registry> as Layer<Registry>>::with_filter(
         tracing_subscriber::fmt::layer(),
         EnvFilter::from_default_env(),
     );
-    let _ = if let Ok(journald) = tracing_journald::layer() {
-        tracing_subscriber::registry()
-            .with(
-                fmt.and_then(<tracing_journald::Layer as Layer<Registry>>::with_filter(
-                    journald,
-                    EnvFilter::from_default_env(),
-                )),
-            )
-            .try_init()
-    } else {
-        tracing_subscriber::registry().with(fmt).try_init()
-    };
+    let _ = tracing_subscriber::registry().with(fmt).try_init();
 }
