f0ff4750d231d66af93356c672909356184cbf2c
Michał Jaremko <msiergiej85@gmail.com>
2024-05-17 08:19:48 +0200

    fix: installer shall require apptainer to be installed

--- bv/src/installer.rs
+++ bv/src/installer.rs
@@ -480,7 +480,14 @@ impl<T: Timer, S: BvService> Installer<T, S> {
 
 async fn check_cli_dependencies() -> Result<()> {
     // smoke test for all CLI tools used in BV
-    for cmd in ["pigz", "tar", "fallocate", "debootstrap", "systemctl"] {
+    for cmd in [
+        "apptainer",
+        "pigz",
+        "tar",
+        "fallocate",
+        "debootstrap",
+        "systemctl",
+    ] {
         bv_utils::cmd::run_cmd(cmd, ["--version"]).await?;
     }
     for cmd in ["ip", "mkfs.ext4"] {
